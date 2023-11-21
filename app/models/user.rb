ba5df5c54d727819077da6a0e49188e506ed16e3
MenendezON <menendezon@gmail.com>
2023-11-21 18:27:16 +0000

    Create the inventory and update the user models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,13 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  has_many :recipes, dependent: :destroy
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+
+  has_many :recipes, dependent: :destroy
+  has_many :inventories, dependent: :destroy
+       
+  validates :name, presence: true
 end
