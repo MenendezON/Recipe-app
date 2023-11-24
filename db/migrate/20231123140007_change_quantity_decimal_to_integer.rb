91e5c6e594bf528f47d694ce6a2b2bded48e90a3
Haider <malikhaiderkhan81@gmail.com>
2023-11-24 17:01:00 +0500

    remove show button and apply it to recipe item

--- db/migrate/20231123140007_change_quantity_decimal_to_integer.rb
+++
@@ -1,6 +0,0 @@
-class ChangeQuantityDecimalToInteger < ActiveRecord::Migration[7.1]
-  def change
-    change_column :inventory_foods, :quantity, :integer
-    change_column :recipe_foods, :quantity, :integer
-  end
-end
